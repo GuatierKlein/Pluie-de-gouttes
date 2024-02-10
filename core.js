71ba437b0c823434bd4c333f114a7d3883b3dbff
Guatier.Klein <klein_gautier@yahoo.Fr>
2024-02-10 21:01:53 +0100

    physique de l'urine et eau

--- core.js
+++ core.js
@@ -1,9 +1,17 @@
 const pixelMatrix = new Array()
 let pipiCounter = 0
+let mouseTimeOut
+const urinePool = document.getElementById("urine")
+let urineLevel = 0
+let ticks = 0;
 
 document.getElementById("button").onclick = () => applyBackground()
+initBottle()
+addUrineLevel(100)
 createFrame(100, 100)
 setInterval(() => tick(), 50)
+
+document.body.addEventListener("mouseup", removeMouseInterval)
 
 function createFrame(width, height) {
     const frame = document.getElementById("frame")
@@ -19,37 +27,155 @@ function createFrame(width, height) {
             line.appendChild(pixel)
             pixelVector.push(pixel)
             pixel.touched = false
+            pixel.locked = false
+            pixel.new = false
+            pixel.beingClicked = false
 
-            pixel.addEventListener("click", (e) => {
-                e.target.style.backgroundColor = "yellow";
-                e.target.touched = true
-                pipiCounter++
-                document.getElementById("counter").innerHTML = pipiCounter
+            pixel.addEventListener("mousedown", (e) => {
+                let occCounter = 0
+                mouseTimeOut = setInterval(() => {
+                    occCounter++
+                    if(occCounter > 20) return
+                    mouseDown(e.target)
+                }, 100);
             })
         } 
         pixelMatrix.push(pixelVector)   
     }
 }
 
+function mouseDown(pixel) {
+    if(urineLevel <= 0) return
+    pixel.beingClicked = true
+    pipiCounter++
+    document.getElementById("counter").innerHTML = pipiCounter
+    touchPixel(pixel, false)
+    addUrineLevel(-1)
+}
+
+function mouseUp(pixel) {
+    pixel.beingClicked = false
+}
+
+function removeMouseInterval() {
+    if(mouseTimeOut) window.clearInterval(mouseTimeOut)
+    mouseTimeOut = false
+}
+
+function addUrineLevel(amount) {
+    if(urineLevel + amount < 0 || urineLevel + amount > 100) return
+    urineLevel += amount
+    urinePool.style.height = `${(urineLevel / 100) * 500}px` 
+}
+
 //louis ta race
 
 function tick() {
-    console.log("tick")
+    ticks++
+    if(ticks % 10 == 0) {
+        ticks = 0
+        addUrineLevel(1)
+    }
+    
     for (let i = pixelMatrix.length - 1; i >= 0; i--) {
         for (let j = pixelMatrix[i].length - 1; j >= 0; j--) {
-            if(!pixelMatrix[i][j].touched) continue 
+            if(!pixelMatrix[i][j].touched || pixelMatrix[i][j].locked) continue 
+            if(pixelMatrix[i][j].new) {
+                pixelMatrix[i][j].new = false
+                continue
+            }
 
             //au sol 
-            if(i === pixelMatrix.length - 1) continue; 
+            if(i === pixelMatrix.length - 1) {
+                lockPixel(pixelMatrix[i][j])
+                continue
+            }
             //un en dessous 
-            if(pixelMatrix[i+1][j].touched) continue
+            if(pixelMatrix[i+1][j].touched) {
+                flowToClosestEmpty(i, j)
+                continue
+            }
+
             //tombe 
-            pixelMatrix[i][j].touched = false 
-            pixelMatrix[i+1][j].touched = true
-            pixelMatrix[i][j].style.backgroundColor = "rgba(255,211,62,0.1)";
-            pixelMatrix[i+1][j].style.backgroundColor = "rgb(255,220,101)";
+            unTouchPixel(pixelMatrix[i][j])
+            touchPixel(pixelMatrix[i + 1][j], false)
         }
     }
+}
+
+function flowToSide(i, j) {
+
+    return true
+}
+
+function flowToClosestEmpty(iStart, jStart) {
+    let distLeft = 0
+    let distRight = 0
+    //right
+    let rightCounter = 0;
+    for (let j = jStart + 1; j < pixelMatrix[iStart].length; j++) {
+        rightCounter++
+        if(!pixelMatrix[iStart + 1][j].touched) {
+            distRight = rightCounter;
+            // pixelMatrix[iStart + 1][j].style.backgroundColor = "blue";
+            break;
+        }
+    }
+    //left
+    let leftCounter = 0;
+    for (let j = jStart - 1; j >= 0; j--) {
+        leftCounter++
+        if(!pixelMatrix[iStart + 1][j].touched) {
+            distLeft = leftCounter;
+            // pixelMatrix[iStart + 1][j].style.backgroundColor = "red";
+            break;
+        }
+    }
+
+    // console.log(distLeft)
+    // console.log(distRight)
+    //compare 
+    if(distLeft != 0 && distLeft < distRight && distLeft < 20) {
+        console.log("go left")
+        unTouchPixel(pixelMatrix[iStart][jStart])
+        touchPixel(pixelMatrix[iStart][jStart - 1], true)
+        return true
+    } 
+    if(distRight != 0 && distRight < distLeft && distRight < 20) {
+        console.log("go right")
+        unTouchPixel(pixelMatrix[iStart][jStart])
+        touchPixel(pixelMatrix[iStart][jStart + 1], true)
+        return true
+    }
+    if(distRight == distLeft && distRight != 0 && distLeft != 0 && distRight < 20 && distLeft < 20) {
+        console.log("random")
+        if(Math.random() > 0.5) {
+            unTouchPixel(pixelMatrix[iStart][jStart])
+            touchPixel(pixelMatrix[iStart][jStart - 1], true)
+            return true
+        } else {
+            unTouchPixel(pixelMatrix[iStart][jStart])
+            touchPixel(pixelMatrix[iStart][jStart + 1], true)
+            return true
+        }
+    }
+    console.log("locked")
+    lockPixel(pixelMatrix[iStart][jStart])
+    return false
+}
+
+function lockPixel(pixel) {
+    pixel.locked = true
+}
+
+function touchPixel(pixel, isNew) {
+    pixel.touched = true 
+    pixel.new = isNew
+    pixel.style.backgroundColor = "rgb(255,220,101)";
+}
+function unTouchPixel(pixel) {
+    pixel.touched = false 
+    pixel.style.backgroundColor = "rgba(255,211,62,0.1)";
 }
 
 function applyBackground() {
@@ -57,4 +183,14 @@ function applyBackground() {
     const url = document.getElementById("pictureInput").value
     document.getElementById("frame").style.backgroundImage = `url(${url})`
     console.log(url)
+}
+
+function initBottle() {
+    const bottle = document.getElementById("bottle")
+    bottle.addEventListener("click", () => {
+        addUrineLevel(10)
+        if(Math.random() < 0.05) {
+            window.open("https://www.youtube.com/watch?v=NTQQtsaIfpo", "_blank")
+        }
+    })
 }
